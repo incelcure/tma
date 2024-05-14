9f77bead523e5c838c370d73a9b264d7e5806517
incelcure <sergeekoilia99@gmail.com>
2024-05-14 23:18:36 +0300

    removing trash

--- config_reader.py
+++ config_reader.py
@@ -10,16 +10,5 @@ class Settings(BaseSettings):
     BOT_TOKEN: SecretStr
     DATABASE_URL: SecretStr
 
-    # model_config = SettingsConfigDict(
-    #     env_file=f"{ENV_FILE_DIR}/.env",
-    #     env_file_encoding="utf-8"
-    # )
-
-
-# # config = Settings(_env_file=None, _env_file_encoding=None, BOT_TOKEN=os.getenv('BOT_TOKEN'), DATABASE_URL=os.getenv('DATABASE_URL'))
-# print(os.getenv('BOT_TOKEN'))
-# config = Settings(env={"BOT_TOKEN": os.getenv('BOT_TOKEN'), "DATABASE_URL": os.getenv('DATABASE_URL')})
-#
-# config = Settings
 
 config = Settings(BOT_TOKEN=os.getenv('BOT_TOKEN'), DATABASE_URL=os.getenv('DATABASE_URL'))
